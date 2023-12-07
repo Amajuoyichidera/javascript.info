01c300bad0183ffaedaf55394ea909c4af8d32cc
Amajuoyichidera <amajuoyichideradavid@gmail.com>
2023-12-07 18:55:28 +0100

    first version

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ For an empty string or cancelled input, show “Canceled”
 
 // while (true) {
 
-//   let value = +prompt("Enter a number", '');
+//   let value = Number(prompt("Enter a number", ''));
 
 //   if (!value) break; // (*)
 
@@ -124,8 +124,145 @@ For an empty string or cancelled input, show “Canceled”
 // alert( 'Sum: ' + sum );
 
 
-for(num = 0; num < 20; num++) {
-   let odd = num % 2
-   alert(num)
-   alert(odd)
+// for(let num = 1; num < 20; num++) {
+//   if(num % 2 == 0) continue;
+//   alert(num)
+// }
+
+
+// let num = 0;
+
+// for(; num < 10; num++) {
+//   if(num == 2 || num == 4) continue;
+//   alert(num)
+// }
+
+// ODD NUMBERS
+// for (let odd = 0; odd < 20 ; odd++) {
+//   if(odd % 2 == 0) {
+//     continue;
+//   } else {
+//     alert(odd)
+//   }
+// }
+
+
+// EVEN NUMBERS
+// for (let even = 0; even < 20; even++) {
+//   if(even % 2 == 0) {
+//     alert(even)
+//   } else {
+//     continue ;
+//   }
+// }
+
+
+
+// Use the for loop to output even numbers from 2 to 10.
+
+// solution
+// for(let even = 2; even <= 10; even++) {
+//   if(even % 2 == 0) {
+//     alert(even)
+//   }
+// }
+
+
+// Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).
+
+// for (let i = 0; i < 3; i++) {
+//   alert( `number ${i}!` );
+// }
+
+// solution
+
+// let i = 0;
+// while (i < 3) {
+//   alert(`number ${i}!`)
+//   i++;
+// }
+
+
+/*
+Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again.
+The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line.
+*/
+
+// solution
+// do {
+//   num = +prompt('enter number bigger than 100')
+// } while(num <= 100 && num)
+
+/*
+
+Write the code using if..else which would correspond to the following switch:
+
+switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
+
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+}
+
+*/
+
+// solution
+
+// let browser = prompt('enter the browser', '')
+
+// if(browser === 'edge') {
+//   alert( "You've got the Edge!" );
+// } else if(browser === 'chrome' || 'firefox' || 'safari' || 'opera') {
+//   alert( 'Okay we support these browsers too' );
+// } else {
+//   alert( 'We hope that this page looks ok!' );
+// }
+
+
+/* 
+
+Rewrite the code below using a single switch statement:
+
+let a = +prompt('a?', '');
+
+if (a == 0) {
+  alert( 0 );
+}
+if (a == 1) {
+  alert( 1 );
+}
+
+if (a == 2 || a == 3) {
+  alert( '2,3' );
+}
+
+*/
+
+// solution
+
+let a = +prompt('a?', '');
+
+switch (a) {
+  case 0:
+    alert(0)
+    break;
+  case 1:
+    alert(1)
+    break;
+  case 2:
+  case 3:
+    alert( '2,3' )
+    break;100
+  default:
+    alert('wrong')
+    break;
 }
