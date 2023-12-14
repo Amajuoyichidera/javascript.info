022289583e27d3a79370ebc1e6cda7ed0fa5585a
Amajuoyichidera <amajuoyichideradavid@gmail.com>
2023-12-14 14:32:43 +0100

    first version

--- app.js
+++ app.js
@@ -391,8 +391,126 @@ ask(
 
 // solution
 
-let ask = (question, yes, no) => {
-  confirm(question) ? yes() : no()
-}
+// let ask = (question, yes, no) => {
+//   confirm(question) ? yes() : no()
+// }
 
-ask('will David be successful?',() => alert('You agreed'), () => alert('you disagreed'))
+// ask('will David be successful?',() => alert('You agreed'), () => alert('you disagreed'))
+
+// objects
+
+/*
+Write the code, one line for each action:
+
+Create an empty object user.
+Add the property name with the value John.
+Add the property surname with the value Smith.
+Change the value of the name to Pete.
+Remove the property name from the object.
+*/
+
+// solution
+
+// let user = {};
+// user.name = 'john';
+// user.surname = 'smith';
+// user.name = 'pete';
+// delete user.name;
+
+// alert(user.surname)
+
+
+/*
+Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+
+Should work like that:
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+
+alert( isEmpty(schedule) );
+ */
+
+// solution
+
+// let myObj = {}
+
+// function isEmpty(obj) {
+//   for (let key in obj) {
+//     return false
+//   } return true
+// }
+
+// alert(isEmpty(myObj))
+
+/*
+We have an object storing salaries of our team:
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+
+If salaries is empty, then the result must be 0
+*/
+
+// solution
+
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130
+// }
+
+// let sum = 0;
+// for(let key in salaries) {
+//   sum += salaries[key]
+// }
+
+// alert(sum)
+
+
+/*
+Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+
+For instance:
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// after the call
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+};
+*/
+
+// solution
+
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu",
+//   overAll: 500,
+// };
+
+// function multiplyNumeric(object) {
+//   for(let key in object) {
+//     typeof(object[key]) === 'number' ? object[key] *= 2 : object[key]
+//     alert(object[key])
+//   }
+// }
+
+// multiplyNumeric(menu)
