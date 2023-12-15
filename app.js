22607b391038e955ed8dc1e6f33c2c67cff38287
Amajuoyichidera <amajuoyichideradavid@gmail.com>
2023-12-15 10:12:24 +0100

    first version

--- app.js
+++ app.js
@@ -514,3 +514,81 @@ menu = {
 // }
 
 // multiplyNumeric(menu)
+
+
+/*
+Create an object calculator with three methods:
+
+read() prompts for two values and saves them as object properties with names a and b respectively.
+sum() returns the sum of saved values.
+mul() multiplies saved values and returns the result.
+let calculator = {
+  // ... your code ...
+};
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
+*/
+
+// solution
+
+
+// let calculator = {
+//   read() {
+//     calculator.a = +prompt('enter num1',);
+//     calculator.b = +prompt('enter num2',)
+//   },
+
+//   sum() {
+//     return this.a + this.b
+//   },
+
+//   mul() {
+//     return this.a * this.b
+//   }
+// }
+
+// calculator.read();
+// alert( calculator.sum() );
+// alert( calculator.mul() );
+
+
+/*
+Create a constructor function Calculator that creates objects with 3 methods:
+
+read() prompts for two values and saves them as object properties with names a and b respectively.
+sum() returns the sum of these properties.
+mul() returns the multiplication product of these properties.
+For instance:
+
+let calculator = new Calculator();
+calculator.read();
+
+alert( "Sum=" + calculator.sum() );
+alert( "Mul=" + calculator.mul() );
+*/
+
+// solution
+
+function Calculator() {
+  this.read = function() {
+    this.a = +prompt('enter num1',)
+    this.b = +prompt('enter num2',)
+  }
+
+  this.sum = function() {
+    return this.a + this.b
+  }
+
+  this.mul = function() {
+    return this.a * this.b
+  }    
+  }
+
+  let calculator = new Calculator()
+  calculator.read();
+
+  alert(calculator.sum())
+  alert(calculator.mul())
+
